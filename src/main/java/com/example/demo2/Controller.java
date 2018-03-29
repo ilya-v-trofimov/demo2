3741af5340bbaab339fa7913f87946a269778fe7
Ilya Trofimov <itrofimov@octo.com>
2018-03-29 17:07:42 +1100

    Commit#2

---
+++ src/main/java/com/example/demo2/Controller.java
@@ -0,0 +1,13 @@
+package com.example.demo2;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @RequestMapping
+    public String index(){
+        return "HELLO, MY FIRST SPRINGBOOT APP!!!";
+    }
+}
--- src/main/java/com/example/demo2/DemoApplication.java
+++ src/main/java/com/example/demo2/DemoApplication.java
@@ -1,7 +1,12 @@
 package com.example.demo2;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -9,4 +14,20 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+
+	@Bean
+	public CommandLineRunner runner(ApplicationContext context){
+		return new CommandLineRunner() {
+			@Override
+			public void run(String... args) throws Exception {
+				System.out.println("BEANS:");
+				String[] beans = context.getBeanDefinitionNames();
+				Arrays.sort(beans);
+				for (String bean : beans){
+					System.out.println(bean);
+				}
+			}
+		};
+	}
+
 }
